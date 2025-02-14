0f4535266d5446bf25d908639289a80129c3e1b9
Daniil Matveev <d.matveev@voiso.com>
2025-02-14 22:31:58 +0400

    remove validateion key

--- lib/hexlet_code/form_builder.rb
+++ lib/hexlet_code/form_builder.rb
@@ -21,7 +21,6 @@ module HexletCode
     end
 
     def input(key, args = {})
-      validate_key(key)
       input_tag = determine_input_tag(args)
       args = prepare_attributes(key, args)
 
@@ -36,12 +35,6 @@ module HexletCode
 
     private
 
-    def validate_key(key)
-      return if @entity.respond_to?(key)
-
-      raise NoMethodError, "Key or method `#{key}` not found in the provided struct"
-    end
-
     def determine_input_tag(args)
       as_tag = args[:as] || 'string'
       Object.const_get("HexletCode::Inputs::#{as_tag.capitalize}Input")
@@ -49,7 +42,7 @@ module HexletCode
 
     def prepare_attributes(key, args)
       args[:name] = key
-      args[:value] = @entity[key] if @entity
+      args[:value] = @entity.public_send(key)
       args
     end
 
