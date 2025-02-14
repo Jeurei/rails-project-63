f58edda5990a512ec36ba1dc75f153bb53e591cb
Daniil Matveev <d.matveev@voiso.com>
2025-02-14 11:44:59 +0400

    fix: try validate key

--- lib/hexlet_code/form_builder.rb
+++ lib/hexlet_code/form_builder.rb
@@ -21,6 +21,7 @@ module HexletCode
     end
 
     def input(key, args = {})
+      validate_key(key)
       input_tag = determine_input_tag(args)
       args = prepare_attributes(key, args)
 
@@ -34,6 +35,12 @@ module HexletCode
     end
 
     private
+
+    def validate_key(key)
+      return if @entity.respond_to?(key)
+
+      raise NoMethodError, "Key or method `#{key}` not found in the provided struct"
+    end
 
     def determine_input_tag(args)
       as_tag = args[:as] || 'string'
